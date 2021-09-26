38c4b8baebc074b00ec8f01488b4efec797b238b
Ava Drumm <78867261+itzgoldenleonard@users.noreply.github.com>
2021-09-26 20:10:20 +0200

    updated new system script

--- Applications/new_system.bash
+++ Applications/new_system.bash
@@ -1,7 +1,7 @@
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/itzgoldenleonard/dotfiles .dotfiles
-# alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs -I{} rm {}
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
+echo 'alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"' >> ~/.bashrc
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs -I{} rm {}
+config checkout
+config config --local status.showUntrackedFiles no
 
