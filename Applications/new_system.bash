d6391ff68d0ac850f84dc1a3ded49937ac008015
Ava Drumm <78867261+itzgoldenleonard@users.noreply.github.com>
2021-09-26 20:37:05 +0200

    Update new_system.bash

--- Applications/new_system.bash
+++ Applications/new_system.bash
@@ -1,7 +1,6 @@
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/itzgoldenleonard/dotfiles .dotfiles
 echo 'alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"' >> ~/.bashrc
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs -I{} rm {}
-config checkout
+config checkout -f master
 config config --local status.showUntrackedFiles no
 
