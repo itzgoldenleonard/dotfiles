228d2d3e2da4c2d4b8c57969b915af319568c743
Ava Drumm <lmbdrumm@gmail.com>
2021-09-28 16:46:23 +0200

    added fish prompt and theme, nvim config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias "config=git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# Aliases
+alias blender="~/.local/share/Steam/steamapps/common/Blender/blender"
+alias config="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
