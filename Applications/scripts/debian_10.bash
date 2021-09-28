abb3219fe84fbbd9ed900e45d04fa6d130196e53
ava <lmbdrumm@gmail.com>
2021-09-28 16:18:13 +0200

    added debian install script, and moved to a dedicated scripts folder

---
+++ Applications/scripts/debian_10.bash
@@ -0,0 +1,6 @@
+sudo apt install neovim
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+sudo apt update
+sudo apt install fish
+chsh -s /usr/bin/fish
