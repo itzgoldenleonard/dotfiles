0a082049a7ab51b52adcaddfb407ea8338f74265
Ava Drumm <78867261+itzgoldenleonard@users.noreply.github.com>
2022-05-12 13:43:55 +0200

    Update new_system.bash

--- bin/scripts/new_system.bash
+++ bin/scripts/new_system.bash
@@ -1,6 +1,6 @@
 echo ".dotfiles" >> .gitignore
 git clone --bare --single-branch --branch breeze https://github.com/itzgoldenleonard/dotfiles .dotfiles
 echo 'alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"' >> ~/.bashrc
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout -f master
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout -f breeze
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
