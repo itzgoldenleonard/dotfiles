b91256d62555ada82a348e505263a2a11b094ef7
Ava Drumm <lmbdrumm@gmail.com>
2022-05-12 10:07:31 +0200

    more space efficiency

--- .config/zsh/plugInstall.sh
+++ .config/zsh/plugInstall.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
-git clone https://github.com/olivierverdier/zsh-git-prompt ~/.config/zsh/zsh-git-prompt
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/olivierverdier/zsh-git-prompt ~/.config/zsh/zsh-git-prompt
